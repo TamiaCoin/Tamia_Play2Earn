7711d3377f1b838c2cc749a8e6758730e6680d23
itsJakeChambers <kjeanmarie01@gmail.com>
2025-02-19 18:16:02 +0100

    Added first variables

--- programs/tamia_p2e/src/lib.rs
+++ programs/tamia_p2e/src/lib.rs
@@ -8,6 +8,11 @@ mod tamia_p2e {
     use super::*;
 
     pub fn submit_score(ctx: Context<SubmitScore>, score: u64) -> Result<()> {
+        let player = &ctx.account.player;
+        let game_state = &mut ctx.accounts.game_state;
+        let token_mint = &ctx.accounts.token_mint;
+        let player_token_account = &ctx.accounts.player_token_account;
+        let token_program = &ctx.accounts.token_program;
         msg!("");
         Ok(())
     }
